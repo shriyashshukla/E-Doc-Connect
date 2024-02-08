e21bbb1750b5e09709e8031654b3eb719d96f149
shriyashshukla <harishukla797@gmail.com>
2024-02-08 16:02:57 +0530

    ...

--- frontend2/src/app/page.jsx
+++ frontend2/src/app/page.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import styles from "./page.module.css";
 import Image from '../../public/hero-bg.jpg';
 
@@ -6,13 +6,67 @@ import Image from '../../public/hero-bg.jpg';
 
 export default function Home() {
   return (
-   
+    <>
+      <div className="page-background">
+        <img src="hero-bg.jpg" alt="Hero Background" className="centered-image" />
+        <div className="left-content">
+          <h1 className="centered-text">Welcome to the MedAssure</h1>
+          <p className="centered-text2">Your Health, Our Assurance</p>
+          <button className="get-started-button">Get Started</button>
+        </div>
 
-  <div className="page-background "  >
-      <img src="hero-bg.jpg" alt="" />
-      
-    </div>
-      
+      </div>
+
+      <div className="main-card-container">
+        <div className="Main-card">
+          <a className="Main-card1" href="#">
+            <p>This is heading</p>
+            <p className="small">
+              Card description with lots of great facts and interesting details.
+            </p>
+            <div className="go-corner" href="#">
+              <div className="go-arrow">→</div>
+            </div>
+          </a>
+        </div>
+        <div className="Main-card">
+          <a className="Main-card1" href="#">
+            <p>This is heading</p>
+            <p className="small">
+              Card description with lots of great facts and interesting details.
+            </p>
+            <div className="go-corner" href="#">
+              <div className="go-arrow">→</div>
+            </div>
+          </a>
+        </div>
+        <div className="Main-card">
+          <a className="Main-card1" href="#">
+            <p>This is heading</p>
+            <p className="small">
+              Card description with lots of great facts and interesting details.
+            </p>
+            <div className="go-corner" href="#">
+              <div className="go-arrow">→</div>
+            </div>
+          </a>
+        </div>
+        <div className="Main-card">
+          <a className="Main-card1" href="#">
+            <p>This is heading</p>
+            <p className="small">
+              Card description with lots of great facts and interesting details.
+            </p>
+            <div className="go-corner" href="#">
+              <div className="go-arrow">→</div>
+            </div>
+          </a>
+        </div>
+      </div>
+   
+    </>
+    
   );
 }
+
 
